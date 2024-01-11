e1b17029588190ebe45b82cedce0338a1edb09a0
Matty0five <matteo.mazzieri.05@gmail.com>
2024-01-11 11:27:30 +0100

    Primo commit

---
+++ src/main/java/com/mycompany/grandprix_mazzieri_mandorlo_pigliautile/GrandPrix_Mazzieri_Mandorlo_Pigliautile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grandprix_mazzieri_mandorlo_pigliautile;
+
+/**
+ *
+ * @author Studente
+ */
+public class GrandPrix_Mazzieri_Mandorlo_Pigliautile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
